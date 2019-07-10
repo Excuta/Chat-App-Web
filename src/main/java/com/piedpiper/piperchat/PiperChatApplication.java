157102b9c962964d9cd60a551e842d480224d543
Yahia Ragae <Yahia.Ragae@intercom.com.eg>
2019-07-10 15:33:11 +0200

    Firs Commit

---
+++ src/main/java/com/piedpiper/piperchat/PiperChatApplication.java
@@ -0,0 +1,13 @@
+package com.piedpiper.piperchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiperChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiperChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/piedpiper/piperchat/PiperChatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.piedpiper.piperchat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PiperChatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
